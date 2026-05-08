13b49c9f506125fb9006037d88e7b7cdfccede01
zhizhou <zhou.zhi.1@outlook.com>
2026-05-09 02:11:42 +0800

    feat: fish shell

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,35 @@
+if status is-interactive
+# Commands to run in interactive sessions can go here
+end
+
+starship init fish | source
+zoxide init fish | source
+direnv hook fish | source
+
+if status is-interactive
+    atuin init fish | source
+end
+
+set -Ux GOPATH $HOME/go
+fish_add_path $GOPATH/bin
+
+fish_add_path /usr/local/go/bin
+
+abbr -a  bat "batcat"
+abbr -a ll "eza --all --long --icons"
+abbr -a la "eza --all --long -1"
+abbr -a l "eza --group-directories-first --icons"
+abbr -a grep "grep --color auto"
+abbr -a apt "nala"
+
+# abbr -a df "df -h
+
+# abbr -a du "du -h"
+
+abbr -a df "duf"
+abbr -a du "ncdu"
+abbr -a help "tldr"
+abbr -a fd "fdfind"
+abbr -a update "sudo nala update && sudo nala upgrade"
+abbr -a td "tealdeer"
+abbr -a tr "tlrc"
